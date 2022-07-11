8ef68a25c7c1145b5490c72de4c6bdc0bf956d2a
Vittorio Francescon <vittorio.francescon@gmail.com>
2022-07-11 10:50:15 +0100

    Added NLOPT test script
    Also added findNLOPT cmake script (taken from
    https://github.com/dartsim/dart/blob/main/cmake/DARTFindNLOPT.cmake)

---
+++ src/nlopt_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <eigen3/Eigen/Geometry>
+#include <eigen3/Eigen/Core>
+
+#include <nlopt.hpp>
+
+using namespace Eigen::placeholders;
+
+int main(void){
+    std::cout << "Nlopt" << std::endl;
+}
